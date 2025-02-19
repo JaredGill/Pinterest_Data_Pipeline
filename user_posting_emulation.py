d44da9d99fdce7c6df9be8e490422d1378403285
Jared Gill <jaredgill96@outlook.com>
2025-02-19 17:02:18 +0000

    Data in JSON format to be sent to Kafka. Currently building function to facilitate this.

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -7,29 +7,66 @@ import json
 import sqlalchemy
 from sqlalchemy import text
 import yaml
+from datetime import datetime
 
-
+# This class likely serves as a connector to interact with an AWS database.
 class AWSDBConnector:
 
-    def __init__(self):
-
-        self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
-        self.USER = 'project_user'
-        self.PASSWORD = ':t%;yCY3Yjg'
-        self.DATABASE = 'pinterest_data'
-        self.PORT = 3306
+    def __init__(self, creds: dict) -> None:
+        self.creds = creds
+        self.HOST = self.creds['RDS_HOST']
+        self.USER = self.creds['RDS_USER']
+        self.PASSWORD = self.creds['RDS_PASSWORD']
+        self.DATABASE = self.creds['RDS_DATABASE']
+        self.PORT = self.creds['RDS_PORT']
         
     def create_db_connector(self):
+        '''The function creates a database connector using SQLAlchemy to connect to a MySQL database.
+        
+        Returns
+        -------
+            The function `create_db_connector` is returning the SQLAlchemy engine object that is created
+        using the connection string for a MySQL database.
+        
+        '''
         engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
         return engine
     
-new_connector = AWSDBConnector()
+def load_yaml(path):
+    with open(path, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
 
-def run_infinite_post_data_loop():
-    while True:
+def convert_datetime_to_string(data):
+    for key, value in data.items():
+        if isinstance(value, datetime):
+            data[key] = value.isoformat() 
+    return data
+
+
+def stream_to_kafka(
+        #invoke_url, 
+        #topic_name, 
+        data):
+    headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
+    #payload = {"records": [{"value": data}]}
+    payload = json.dumps({
+        "records": [
+            {
+            # Data should be send as pairs of column_name:value, with different columns separated by commas       
+            "value": data
+            }
+        ]
+    })
+    print(payload)
+    # response = requests.post(f"{api_url}/topics/{topic_name}", json=payload, headers=headers)
+
+
+
+def run_infinite_post_data_loop(engine):
+    for i in range(500):
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
-        engine = new_connector.create_db_connector()
 
         with engine.connect() as connection:
 
@@ -38,16 +75,37 @@ def run_infinite_post_data_loop():
             
             for row in pin_selected_row:
                 pin_result = dict(row._mapping)
+                cleaned_pin_result = convert_datetime_to_string(pin_result)  
+                print('Pin row:')
+                stream_to_kafka(cleaned_pin_result)
 
-
-        geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
+            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
-            
+                
             for row in geo_selected_row:
                 geo_result = dict(row._mapping)
+                cleaned_geo_result = convert_datetime_to_string(geo_result)  
+                print('Geo row:')
+                stream_to_kafka(cleaned_geo_result)
 
-        user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
+            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
-            
+                
             for row in user_selected_row:
                 user_result = dict(row._mapping)
+                cleaned_user_result = convert_datetime_to_string(user_result)  
+                print('User row')
+                stream_to_kafka(cleaned_user_result)
+        break
+
+if __name__ == "__main__":
+    pinterest_rds_creds = load_yaml('pinterest_data_rds.yaml')
+    #print(pinterest_rds_creds)
+
+    new_connector = AWSDBConnector(pinterest_rds_creds)
+    engine = new_connector.create_db_connector()
+    run_infinite_post_data_loop(engine)
+
+    api_creds = load_yaml('API_invoke_url.yaml')
+    api_invoke_url = api_creds['invoke_url']
+    print(api_invoke_url)
