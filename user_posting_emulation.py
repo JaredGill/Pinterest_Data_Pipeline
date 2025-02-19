9a94c39a4998fb4b9a9e9d48d7b28fb0e190e37e
Jared Gill <jaredgill96@outlook.com>
2025-02-19 22:40:55 +0000

    Data sent to Kafka API REST proxy 1 row at a time for it to be forwaded into the S3 bucket to emulate Pinterest Data.

--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -40,31 +40,42 @@ def load_yaml(path):
 def convert_datetime_to_string(data):
     for key, value in data.items():
         if isinstance(value, datetime):
-            data[key] = value.isoformat() 
+            data[key] = value.isoformat()
     return data
 
 
 def stream_to_kafka(
-        #invoke_url, 
-        #topic_name, 
+        invoke_url, 
+        topic_name, 
         data):
+    
     headers = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
-    #payload = {"records": [{"value": data}]}
+
     payload = json.dumps({
         "records": [
-            {
-            # Data should be send as pairs of column_name:value, with different columns separated by commas       
-            "value": data
-            }
-        ]
-    })
-    print(payload)
-    # response = requests.post(f"{api_url}/topics/{topic_name}", json=payload, headers=headers)
+            {"value": data}
+            ]
+        })
+    
+    #print(payload)
+    full_invoke_url = f"{invoke_url}/topics/{topic_name}"
+    #print(full_invoke_url)
+    response = requests.request("POST", full_invoke_url, headers=headers, data=payload)
+
+    if response.status_code == 200:
+        print(f"Data successfully sent to topic {topic_name}")
+    else:
+        print(f"Failed to send data to topic {topic_name}: {response.status_code} - {response.text}")
 
 
+def run_infinite_post_data_loop(engine, api_details):
+    api_invoke_url = api_details['invoke_url']
+    pin_topic = api_details['pin_topic']
+    geo_topic = api_details['geo_topic']
+    user_topic = api_details['user_topic']
 
-def run_infinite_post_data_loop(engine):
     for i in range(500):
+        print(i)
         sleep(random.randrange(0, 2))
         random_row = random.randint(0, 11000)
 
@@ -77,7 +88,7 @@ def run_infinite_post_data_loop(engine):
                 pin_result = dict(row._mapping)
                 cleaned_pin_result = convert_datetime_to_string(pin_result)  
                 print('Pin row:')
-                stream_to_kafka(cleaned_pin_result)
+                stream_to_kafka(api_invoke_url, pin_topic, cleaned_pin_result)
 
             geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
             geo_selected_row = connection.execute(geo_string)
@@ -86,26 +97,24 @@ def run_infinite_post_data_loop(engine):
                 geo_result = dict(row._mapping)
                 cleaned_geo_result = convert_datetime_to_string(geo_result)  
                 print('Geo row:')
-                stream_to_kafka(cleaned_geo_result)
+                stream_to_kafka(api_invoke_url, geo_topic, cleaned_geo_result)
 
             user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
             user_selected_row = connection.execute(user_string)
                 
             for row in user_selected_row:
                 user_result = dict(row._mapping)
-                cleaned_user_result = convert_datetime_to_string(user_result)  
+                cleaned_user_result = convert_datetime_to_string(user_result)
                 print('User row')
-                stream_to_kafka(cleaned_user_result)
-        break
+                stream_to_kafka(api_invoke_url, user_topic, cleaned_user_result)
+        
+        
 
 if __name__ == "__main__":
     pinterest_rds_creds = load_yaml('pinterest_data_rds.yaml')
     #print(pinterest_rds_creds)
+    api_creds = load_yaml('API_invoke_url.yaml')
 
     new_connector = AWSDBConnector(pinterest_rds_creds)
     engine = new_connector.create_db_connector()
-    run_infinite_post_data_loop(engine)
-
-    api_creds = load_yaml('API_invoke_url.yaml')
-    api_invoke_url = api_creds['invoke_url']
-    print(api_invoke_url)
+    run_infinite_post_data_loop(engine, api_creds)
