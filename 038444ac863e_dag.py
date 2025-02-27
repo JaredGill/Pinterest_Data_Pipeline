c47099bbb7a6155e21b723fc36684b7ff86ed7b5
Jared Gill <jaredgill96@outlook.com>
2025-02-27 00:05:03 +0000

    Airflow DAG to run the Databricks ETL.ipynb script daily. This was uploaded to the shared s3 bucket to populate in the Managed Apache Airflow environment from which it was testing using the Airflow UI.

---
+++ 038444ac863e_dag.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+notebook_task = {
+    'notebook_path': '/Workspace/Users/name@email_provider/ETL',
+}
+
+notebook_params = {
+    "Variable":5
+}
+
+default_args = {
+    'owner': '038444ac863e',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+initial_start_date = datetime(day=25, month=2, year=2025)
+with DAG('038444ac863e_dag',
+    start_date=initial_start_date,
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
